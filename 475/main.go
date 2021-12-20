e180707dd1ca7b227e9253243e46e978d58f1a45
xiang.zhang <acjason@163.com>
2021-12-20 12:42:48 +0800

    add

--- 475/main.go
+++ 475/main.go
@@ -2,54 +2,48 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"sort"
 )
 
-// 每个房子找到最近的加热器，即为最小半径
-// 0: 什么都没
-// 1: 有房子
-// 2: 有加热器
-// 3: 有房子和加热器
 func findRadius(houses []int, heaters []int) int {
-	minRadius := math.MaxInt32
 	sort.Ints(houses)
 	sort.Ints(heaters)
-	point := make(map[int]byte)
-	mixed := make([]int, 60000)
-	index := 0
-	for i, j := 0, 0; i < len(houses) || j < len(heaters); index++ {
-		if j == len(heaters) && i < len(houses) || houses[i] > heaters[j] {
-			mixed[index] = houses[i]
-			point[mixed[index]] += 1
-			i++
-			continue
-		}
-		if i == len(houses) && j < len(heaters) || houses[i] <= heaters[j] {
-			mixed[index] = heaters[j]
-			point[mixed[index]] += 2
-			j++
-			continue
+	maxRadius := -1
+	for _, v := range houses {
+		closetHeaterDistance := binarySearch(heaters, 0, len(heaters)-1, v)
+		// fmt.Println(closetHeaterDistance)
+		if closetHeaterDistance > maxRadius {
+			maxRadius = closetHeaterDistance
 		}
 	}
+	return maxRadius
+}
 
-	// findHeaterRadius(house int)
-	for k, v := range mixed {
-		// has house
-		if point[v]%2 == 1 {
-
+func binarySearch(heaters []int, a int, b int, value int) int {
+	gap := b - a
+	if gap < 2 {
+		left := value - heaters[a]
+		if left < 0 {
+			left = -left
 		}
+		right := heaters[b] - value
+		if right < 0 {
+			right = -right
+		}
+		if left < right {
+			return left
+		}
+		return right
 	}
-	return 1
+	mid := (a + b) / 2
+	if heaters[mid] > value {
+		return binarySearch(heaters, a, mid, value)
+	}
+	return binarySearch(heaters, mid, b, value)
 }
-func findHeaterRadius(house int) {
 
-}
-
-// 输入: houses = [1,2,3,4], heaters = [1,4]
-// 输出: 1
 func main() {
-	houses := []int{1, 2, 3, 4}
-	heaters := []int{1, 4}
+	houses := []int{1, 999}
+	heaters := []int{499, 500, 501}
 	fmt.Println(findRadius(houses, heaters))
 }
